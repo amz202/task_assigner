3d08c34cd2c3d3e27808e100779c2da5ce4bc9cd
amz202 <abdulmajidzeeshan4@gmail.com>
2025-06-26 10:57:09 +0500

    support for expo mobile

--- middleware/validateToken.ts
+++ middleware/validateToken.ts
@@ -1,59 +1,65 @@
-import { Request, Response, NextFunction } from 'express';
+import {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
-import { db } from '../data/db';
-import { eq  } from 'drizzle-orm';
-import { UserTable } from '../data/schema';
+import {db} from '../data/db';
+import {eq} from 'drizzle-orm';
+import {UserTable} from '../data/schema';
 
 const secret = process.env.JWT_SECRET || "mysecretkey";
 
 export interface AuthRequest extends Request {
-  user?: any;
+    user?: any;
 }
 
 const validateToken = async (req: AuthRequest, res: Response, next: NextFunction) => {
 
-  const token = req.cookies?.jwt;
+    // const token = req.cookies?.jwt;
+    const token = req.cookies?.jwt || req.headers?.authorization?.split(' ')[1];
 
-  if (!token) {
-     res.status(401).json({ message: 'Access token missing' });
-     return
-  }
+    if (!token) {
+        res.status(401).json({message: 'Access token missing'});
+        return
+    }
 
-  try {
-      
-
-    const decoded = jwt.verify(token, secret);
-    const {userId} = decoded as { userId: number };
-      const user = await db
-      .select({
-      id: UserTable.id,
-      name: UserTable.name,
-      email: UserTable.email,
-      role: UserTable.role,
-      is_approved: UserTable.isApproved,
-      created_at: UserTable.createdAt,
-      updated_at: UserTable.updatedAt,
-      })
-      .from(UserTable)
-      .where(eq(UserTable.id, userId))
-      .limit(1);  
-      req.user = user[0];
-      
+    try {
+        const decoded = jwt.verify(token, secret);
+        const {userId} = decoded as { userId: number };
+        const user = await db
+            .select({
+                id: UserTable.id,
+                name: UserTable.name,
+                email: UserTable.email,
+                role: UserTable.role,
+                is_approved: UserTable.isApproved,
+                created_at: UserTable.createdAt,
+                updated_at: UserTable.updatedAt,
+            })
+            .from(UserTable)
+            .where(eq(UserTable.id, userId))
+            .limit(1);
+        req.user = user[0];
 
 
-  res.cookie("jwt" , token , {
-        maxAge : 7 * 24 * 60 * 60 *1000,
-        httpOnly : true,
-        sameSite : "strict" , 
-       
+        // res.cookie("jwt" , token , {
+        //       maxAge : 7 * 24 * 60 * 60 *1000,
+        //       httpOnly : true,
+        //       sameSite : "strict" ,
+        //
+        //
+        //   })
 
-    })
+        if (req.cookies?.jwt) {
+            res.cookie("jwt", token, {
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                sameSite: "strict",
+            });
+        }
 
-    next(); 
-  } catch (err) {
-     res.status(403).json({ message: 'Invalid or expired token' });
-     return
-  }
+        next();
+    } catch (err) {
+        res.status(403).json({message: 'Invalid or expired token'});
+        return
+    }
 };
 
 export default validateToken;
